f61f9406cfcc2faf1bf78c3153137cca1afef124
reesly <reesly@gmail.com>
2022-02-10 17:40:12 +0300

    123213

--- assets/js/jqcart.js
+++ assets/js/jqcart.js
@@ -212,7 +212,7 @@
 			
       if (tx != '') { pars = pars + '&answer_long_text_1877903=' + encodeURIComponent(tx + '\n- '); }
 
-      pars = 'https:' + '/' + '/forms.yandex.ru/u/5e5831b56296cf035c59867f/?iframe=1' + pars+'&answer_url_13308679=korzina';
+      pars = 'https:' + '/' + '/forms.yandex.ru/u/5e5831b56296cf035c59867f/?iframe=1' + pars+'&answer_short_text_13331990=korzina';
       document.getElementById('form1').src = pars;
 
       actions.hideCart();
